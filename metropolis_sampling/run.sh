9d4fe178f4eda88d02aa19f806682980043b627f
Thomas Mejer Hansen <tmeha@arizona.localdomain>
2020-12-11 09:03:57 +0100

    single thread Matlab

--- metropolis_sampling/run.sh
+++ metropolis_sampling/run.sh
@@ -1,5 +1,7 @@
 # Run matlab
 matlab -batch "ex_tomo_metropolis; exit"
+# Run matlab
+matlab  -singleCompThread -batch "ex_tomo_metropolis; exit"
 # Run Julia
 julia ex_tomo_metropolis.jl
 # Run Python
